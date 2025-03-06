7a6c03ce3007c1d5c892f68926aa8ffe7312184a
techswarn <tech.swarns@gmail.com>
2025-03-06 23:32:38 +0530

    code update

--- test-frontend/src/App.jsx
+++ test-frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [health, setHealth] = useState('')
   console.log(health)
   useEffect( () => {
-    axios.get('http://127.0.0.1:3000/api/v1/health')
+    axios.get('https://test-app-ipnjc.ondigitalocean.app/backend/api/v1/health')
       .then(response => {
         setHealth(response.data);
       })
