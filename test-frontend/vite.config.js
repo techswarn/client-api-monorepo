0102034f816b3f8345be1589abbf984c21ebebdf
techswarn <tech.swarns@gmail.com>
2025-03-06 23:53:48 +0530

    code update

--- test-frontend/vite.config.js
+++ test-frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    allowedHosts: 'test.techenv.dev'
+  server: {
+    allowedHosts: ['test.techenv.dev'] 
   }
 })
